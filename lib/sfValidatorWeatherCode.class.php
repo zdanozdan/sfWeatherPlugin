be9be43e3b28bc64d041c7f412727c9db36fb991
Tomasz Zdanowski <tomasz@amilo.(none)>
2010-11-09 15:21:16 +0100

    added validator

---
+++ lib/sfValidatorWeatherCode.class.php
@@ -0,0 +1,23 @@
+<?php
+
+class sfValidatorWeatherCode extends sfValidatorBase
+{
+  protected function configure($options = array(), $messages = array())
+  {
+    $this->setMessage('invalid', 'City with code "%address%" could not be found in weather.com database');
+  }
+
+  /**
+   * @see sfValidatorBase
+   */
+  protected function doClean($value)
+  {
+    $w = new sfWeather($value);
+    if(count($w->retrieve())
+      {
+	return $value;
+      }
+
+    throw new sfValidatorError($this, 'invalid',array('city_code'=>$value));
+  }
+}
